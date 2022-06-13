221345553030dc9d802c162a19c6977e57e11fea
Ahmet Faruk Sırma <ahmetfaruk2000@windowslive.com>
2022-06-13 17:00:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,22 @@ import time
 import firebase_admin
 from firebase_admin import db
 from datetime import datetime
-
+import RPi.GPIO as GPIO
+import time
 
 cred_obj = firebase_admin.credentials.Certificate('key.json')
 default_app = firebase_admin.initialize_app(cred_obj, {
     'databaseURL': 'https://ytu-surucu-destek-sistemi-default-rtdb.europe-west1.firebasedatabase.app/'
 })
+
+GPIO.setmode(GPIO.BCM)
+TRIG = 23
+ECHO = 24
+
+print ("Distance Measurement in progress...")
+
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
 
 ref = db.reference("/")
 
@@ -18,18 +28,46 @@ running = True
 
 def getPositionData(gps):
     nx = next(gpsd)
+    GPIO.output(TRIG, False)
+
+    print("Waiting For Sensor")
+    time.sleep(2)
+
+    GPIO.output(TRIG, True)
+    time.sleep(0.00001)
+
+    GPIO.output(TRIG, False)
+
+    while GPIO.input(ECHO) == 0:
+        pulse_start = time.time()
+
+    while GPIO.input(ECHO) == 1:
+        pulse_end = time.time()
+
+    pulse_duration = pulse_end - pulse_start
+
+    distance = pulse_duration * 17150
+    distance = round(distance, 2)
+
     if nx['class'] == 'TPV':
         now = datetime.now()
         current_time = now.strftime("%H:%M:%S")
         latitude = getattr(nx, 'lat', "Unknown")
+        # $GPRMC,123519,A, 4807.038,N, 01131.000,E, 022.4, 084.4,230394,003.1,W*6A
         longitude = getattr(nx, 'lon', "Unknown")
+        speed = getattr(nx, 'speed', "Unknown")
         rasp_key.update(
             {
                 'latitude': latitude,
                 'longitude': longitude,
-                'time': current_time
+                'speed': speed,
+                'time': current_time,
+                'distance': distance
         })
-        print(("Konumunuz: lon = " + str(longitude) + ", lat = " + str(latitude)))
+        print(("Konumunuz: lon = " + str(longitude) + ", lat = " + str(latitude)
+               + "Zaman: " + str(current_time)
+               + "Hız: " + str(speed))
+               + " Uzaklık: " + str(distance))
 
 
 gpsd = gps(mode = WATCH_ENABLE | WATCH_NEWSTYLE)

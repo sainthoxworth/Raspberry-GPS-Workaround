0835e3f73e3097278cfee9d436f78f811ed4b3e2
Ahmet Faruk Sırma <ahmetfaruk2000@windowslive.com>
2022-06-13 19:38:38 +0300

    Update main.py
    Added new lines of code for the second prox sensor

--- main.py
+++ main.py
@@ -15,10 +15,16 @@ GPIO.setmode(GPIO.BCM)
 TRIG = 23
 ECHO = 24
 
+TRIG1 = 20
+ECHO1 = 21
+
 print ("Distance Measurement in progress...")
 
 GPIO.setup(TRIG, GPIO.OUT)
 GPIO.setup(ECHO, GPIO.IN)
+
+GPIO.setup(TRIG1, GPIO.OUT)
+GPIO.setup(ECHO1, GPIO.IN)
 
 ref = db.reference("/")
 
@@ -37,17 +43,35 @@ def getPositionData(gps):
     time.sleep(0.00001)
 
     GPIO.output(TRIG, False)
+    
+    GPIO.output(TRIG1, True)
+    time.sleep(0.00001)
+
+    GPIO.output(TRIG1, False)
 
     while GPIO.input(ECHO) == 0:
         pulse_start = time.time()
 
     while GPIO.input(ECHO) == 1:
         pulse_end = time.time()
+        
+    while GPIO.input(ECHO1) == 0:
+        pulse_start1 = time.time()
+
+    while GPIO.input(ECHO1) == 1:
+        pulse_end1 = time.time()
 
     pulse_duration = pulse_end - pulse_start
 
     distance = pulse_duration * 17150
     distance = round(distance, 2)
+    
+    pulse_duration1 = pulse_end1 - pulse_start1
+
+    distance1 = pulse_duration1 * 17150
+    distance1 = round(distance1, 2)
+    
+    print (distance1)
 
     if nx['class'] == 'TPV':
         now = datetime.now()
